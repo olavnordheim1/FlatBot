fa0dbd9843b65405a543bc6f9d604dd4ed3d857c
Marco Chinello <marco.chinello@gmail.com>
2024-12-16 17:52:19 +0000

    added logging to main

--- main.py
+++ main.py
@@ -1,32 +1,48 @@
 import os
 import time
+from datetime import datetime
 import random
 import importlib
+import logging
 from modules.Database import ExposeDB, Expose
 from modules.EmailFetcher import EmailFetcher
-from modules.StealthBrowser import StealthBrowser
+
+
+logger = logging.getLogger(__name__)
+log_level = logging.INFO
+
+def init_log():    # Create logs directory if it doesn't exist
+    log_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "logs")
+    os.makedirs(log_dir, exist_ok=True)
+    # Generate log file name
+    current_time = datetime.now()
+    log_file = f"Flatbot_{current_time.month}_{current_time.year}.log"
+    log_file_path = os.path.join(log_dir, log_file)
+    logging.basicConfig(filename=log_file_path, level=log_level)
 
 
 def main():
+    init_log()
+    logger.debug('Log started')
     print("Initializing the database...")
     db_instance = ExposeDB()
-    print("Database initialized successfully!")
-    print("Fetching emails...")
+    logger.info("Database initialized successfully!")
+    logger.debug("Fetching emails...")
     email_processor = EmailFetcher(db_instance)
     email_processor.fetch_emails()
-    print("Email fetching completed!")
+    logger.info("Email fetching completed!")
 
-    print("Starting processor...")
+    logger.debug("Starting processor...")
     exposes = db_instance.get_unprocessed_exposes()
     if not exposes:
-        print("No unprocessed exposes found.")
+        logger.info("No unprocessed exposes found.")
         return
     for expose in exposes:
         try:
             processor_module = importlib.import_module(f"modules.{expose.source}_processor")
             processor_class = getattr(processor_module, f"{expose.source}_processor", None)
             if not processor_class:
-                print(f"Processor class for {expose.source} not found")
+                logger.error(f"Processor class for {expose.source} not found")
                 continue
 
             processor_instance = processor_class()
@@ -35,18 +51,18 @@ def main():
                 db_instance.update_expose(expose)
                 
         except ModuleNotFoundError:
-            print(f"Processor module for {expose.source} not found")
+            logger.error(f"Processor module for {expose.source} not found")
         except AttributeError as e:
-            print(f"Error accessing processor class: {e}")
+            logger.error(f"Error accessing processor class: {e}")
         except Exception as e:
-            print(f"Error processing expose from {expose.source}: {e}")
+            logger.error(f"Error processing expose from {expose.source}: {e}")
 
-    print("All new exposes processed.")
+    logger.info("All new exposes processed.")
     random_wait(300, 900)
 
 def random_wait(min_seconds=2, max_seconds=5):
     wait_time = random.uniform(min_seconds, max_seconds)
-    print(f"Waiting for {wait_time:.2f} seconds...")
+    logger.debug(f"Waiting for {wait_time:.2f} seconds...")
     time.sleep(wait_time)
 
 ############################################################
